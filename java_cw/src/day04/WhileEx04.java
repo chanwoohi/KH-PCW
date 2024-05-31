2dcd3851067c05eba07c821de7072ee858e8275e
chanwoohi <chanwoohi@naver.com>
2024-05-31 12:42:17 +0900

    while 문 예제 4

---
+++ java_cw/src/day04/WhileEx04.java
@@ -0,0 +1,28 @@
+package day04;
+
+public class WhileEx04 {
+		//while 문 예제 4
+	public static void main(String[] args) {
+		/*	1부터 10까지 합을 구하는 코드를 작성하세요.
+		 * 
+		 *  	sum = 0
+		 * i=1	sum = sum + 1
+		 * i=2	sum = sum + 2  
+		 * i=3	sum = sum + 3
+		 * ...
+		 * i=10	sum = sum + 10
+		 * 반복횟수 : i는 1부터 10까지 1씩 증가.
+		 * 규칙성 : sum = sum + i
+		 * 반복문 종료 후 : sum 을 출력
+		 * */
+		int i = 1;
+		int sum = 0;
+		while( i <= 10 ) {
+			sum += i;
+			++i;
+		}
+		
+		System.out.println("1부터 10까지 합 : " + sum);
+	}
+
+}
