6581726da87261b640b21d7b299b8400d8d3a6f8
chanwoohi <chanwoohi@naver.com>
2024-06-03 12:45:14 +0900

    라벨 예제 1

---
+++ java_cw/src/day05/LabelEx01.java
@@ -0,0 +1,40 @@
+package day05;
+
+public class LabelEx01 {
+	// 라벨 예제 1
+	public static void main(String[] args) {
+		int i, j;
+		
+		for( i = 1 ; i <= 5 ; i++ ) {
+			for( j = 1 ; j <= 5 ; j++ ) {
+				System.out.print(i*j + " ");
+			}
+			System.out.println();
+		}
+		
+		//한 줄에 숫자를 3개씩으로 수정, break문을 활용해서
+		for( i = 1 ; i <= 5 ; i++ ) {
+			for( j = 1 ; j <= 5 ; j++ ) {
+				System.out.print(i*j + " ");
+				if( j == 3 ) {
+					break;
+				}
+			}
+			System.out.println();
+		}
+		
+		//라벨 이름 A
+		A : for( i = 1 ; i <= 5 ; i++ ) {
+				for( j = 1 ; j <= 5 ; j++ ) {
+					System.out.print(i*j + " ");
+					if( j == 3 ) {
+						break A;
+					}
+				}
+				System.out.println();
+			}
+		
+
+	}
+
+}
