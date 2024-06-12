cacdcde9e0981e3b31a570764976b0c56ba66781
chanwoohi <chanwoohi@naver.com>
2024-06-12 17:55:54 +0900

    멤버 변수 초기화 순서

---
+++ java_cw/src/day11/InstanceMemberInitEx01.java
@@ -0,0 +1,30 @@
+package day11;
+
+public class InstanceMemberInitEx01 {
+	// 멤버 변수 초기화 순서
+	public static void main(String[] args) {
+		// 객체 생성하지 않아서 static 초기화 블록까지만 초기화 됨
+		System.out.println(B.max);
+		// 객체를 생성할 때 생성자에서 max도 초기화 함
+		B b = new B(); // 이 때 num와 max의 값은?
+		System.out.println(b.num);
+		System.out.println(B.max);
+	
+	}
+}
+
+
+class B{
+	public int num = 10;
+	public static int max = 20;
+	{
+		num = 20;
+	}
+	static {
+		max = 30;
+	}
+	public B() {
+		num = 30;
+		max = 30;
+	}
+}
