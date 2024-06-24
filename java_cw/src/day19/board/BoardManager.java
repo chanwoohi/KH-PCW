27e94ffd08fd18e36a69f0e2a3ba80656ddef4b7
KimHyeonjung <kardel@naver.com>
2024-06-24 17:48:48 +0900

    19일차 과제 보드매니저 수정

--- java_cw/src/day19/board/BoardManager.java
+++ java_cw/src/day19/board/BoardManager.java
@@ -68,6 +68,7 @@ public class BoardManager implements Program{
 	public void save(String fileName) {
 
 	}
+	
 	@Override
 	public void load(String fileName) {
 
@@ -85,15 +86,24 @@ public class BoardManager implements Program{
 			updateBoard();
 			break;
 		case DELETE:
+			deleteBoard();
 			break;
 		case SEARCH:
+			searchBoard();
 			break;
 		case EXIT:
+			exit();
 			break;
 		default:
 		}
 	}
 
+	private void exit() {
+		printBar();
+		System.out.println("프로그램을 종료합니다.");
+		printBar();
+		
+	}
 	private void insertBoard() {
 		//게시글 정보를 입력
 		//입력한 정보를 이용해서 게시글 객체를 생성
@@ -105,12 +115,10 @@ public class BoardManager implements Program{
 		//알림문구를 출력
 		printBar();
 		System.out.println(board.getNum() + "번 게시글이 등록 되었습니다.");
-		printBar();
 
 	}
 
 	private Board inputBoard() {
-		printBar();
 		System.out.print("제목 : ");
 		scan.nextLine();//공백 처리
 		String title = scan.nextLine(); // nextLine인 이유? 공백도 포함시키려고 
@@ -127,9 +135,30 @@ public class BoardManager implements Program{
 	private void updateBoard() {
 		printBar();
 		//수정할 게시글 번호를 입력
-		System.out.println("수정할 게시글 번호 입력 : ");
-		int num = 0;
-		num = scan.nextInt();
+		System.out.print("수정할 게시글 번호 입력 : ");
+		int num = scan.nextInt();
+		printBar();
+
+		Board board = selectBoard(num);
+		if(board == null) {
+			return;
+		}
+		printBar();
+		//같으면 새 제목과 내용을 입력
+		scan.nextLine();
+		System.out.print("제목 : ");
+		String title = scan.nextLine();
+		System.out.print("내용 : ");
+		String contents = scan.nextLine();
+		//가져온 객체의 제목과 내용을 입력받은 제목과 내용으로 수정
+		board.update(title, contents);
+		//안내문구 출력
+		printBar();
+		System.out.println(board.getNum() + "번 게시글이 수정되었습니다.");
+
+	}
+
+	private Board selectBoard(int num) {
 		//입력한 게시글 번호를 이용하여 게시글 객체를 생성
 		Board board = new Board(num);
 		//리스트에서 생성한 객체가 몇번지에 있는지 번지를 가져옴
@@ -138,7 +167,7 @@ public class BoardManager implements Program{
 		if((index) < 0) {
 			printBar();
 			System.out.println("등록되지 않거나 삭제된 게시글입니다. ");
-			return;
+			return null;
 		}
 		//아이디, 비번을 입력 받음
 		System.out.print("아이디 : ");
@@ -151,27 +180,58 @@ public class BoardManager implements Program{
 		if(!board.checkWriter(id, pw)) {
 			printBar();
 			System.out.println("아이디 또는 비번이 일치하지 않습니다.");
-			return;
-		}		
-		//같으면 새 제목과 내용을 입력
-		scan.nextLine();
-		System.out.println("제목 : ");
-		String title = scan.nextLine();
-		System.out.println("내용 : ");
-		String contents = scan.nextLine();
-		//가져온 객체의 제목과 내용을 입력받은 제목과 내용으로 수정
-		board.update(title, contents);
-		//안내문구 출력
-		printBar();
-		System.out.println(board.getNum() + "번 게시글이 수정되었습니다.");
-
+			return null;
+		}
+		return board;
 	}
-
 	private void deleteBoard() {
-
+		printBar();
+		//삭제할 게시글 번호를 입력
+		System.out.print("삭제할 게시글 번호 입력 : ");
+		int num = scan.nextInt();
+		printBar();
+		Board board = selectBoard(num);
+		if(board == null) {
+			return;
+		}
+		printBar();
+		//리스트에서 게시글을 삭제
+		list.remove(board);
+		System.out.println(board.getNum() + "번 게시글이 삭제되었습니다.");
 	}
 
 	private void searchBoard() {
-
+		//검색어 입력
+		System.out.print("검색어 입력 : ");
+		String search = scan.nextLine();
+		
+		//게시글에서 검색어가 제목 또는 내용에 들어간 게시글 리스트를 가져옴
+		List<Board> searchList = new ArrayList<Board>();
+		searchList.contains(search);
+		System.out.println(search);
+		//게시글 리스트가 비어 있으면 안내문구 출력 후 종료
+		
+		//가져온 게시글 리스트를 출력
+		
+		//게시글을 확인할건지 선택
+		
+		//확인하지 않겠다고 하면 종료
+		
+		//확인하면 게시글 번호를 입력
+		
+		//입력받은 게시글 번호로 객체를 생성
+		
+		//검색 리스트에서 생성된 객체와 일치하는 번지를 확인
+		
+		//번지가 유효하지 않으면 안내문구 출력 후 종료
+		
+		//번지에 있는 게시글을 가져옴
+		
+		//가져온 게시글을 출력
+		
+		//메뉴로 돌아가려면... 문구 출력
+		
+		//엔터를 입력받도록 처리
+		
 	}
 }
