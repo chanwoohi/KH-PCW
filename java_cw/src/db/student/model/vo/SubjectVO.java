b2b4d8689c7ba0310850c112579f11f2aa21dd0a
ParkChanWooo <chanwoohi@naver.com>
2024-07-26 16:14:42 +0900

    DB 06일차 : 학생 관리 프로그램 - MyBatis - 과목 수정기능까지 구현

---
+++ java_cw/src/db/student/model/vo/SubjectVO.java
@@ -0,0 +1,15 @@
+package db.student.model.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor // 기본 생성자
+public class SubjectVO{
+
+	private String su_key;
+	private int su_grade;
+	private int su_semester;
+	private int su_num;
+
+}
