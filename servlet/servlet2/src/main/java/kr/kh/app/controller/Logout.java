52e8cdd6197f2a147d97a32ee68c5b206d4d1e6b
ParkChanWooo <chanwoohi@naver.com>
2024-08-23 12:10:58 +0900

    서블릿 11일차 : 자동로그인 기능 구현

--- servlet/servlet2/src/main/java/kr/kh/app/controller/Logout.java
+++ servlet/servlet2/src/main/java/kr/kh/app/controller/Logout.java
@@ -7,13 +7,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.kh.app.model.vo.MemberVO;
+import kr.kh.app.service.MemberService;
+import kr.kh.app.service.MemberServiceImp;
+
 
 @WebServlet("/logout")
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
+	private MemberService memberService = new MemberServiceImp();
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//회원의 쿠기 정보를 업데이트 null로
+		MemberVO user = (MemberVO)request.getSession().getAttribute("user");
+		if(user != null) {
+			user.setMe_cookie(null);
+			memberService.updateMemberCookie(user);
+		}
+		
 		//세션에 있는 회원 정보를 지움
 		request.getSession().removeAttribute("user");
 		//알림
--- servlet/servlet2/src/main/java/kr/kh/app/service/MemberService.java
+++ servlet/servlet2/src/main/java/kr/kh/app/service/MemberService.java
@@ -1,5 +1,8 @@
 package kr.kh.app.service;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import kr.kh.app.model.vo.MemberVO;
 
 public interface MemberService {
@@ -9,4 +12,10 @@ public interface MemberService {
 	boolean checkId(String me_id);
 	
 	MemberVO login(MemberVO member);
+
+	Cookie createCookie(MemberVO user, HttpServletRequest request);
+
+	MemberVO getMemberBySid(String sid);
+
+	void updateMemberCookie(MemberVO user);
 }
