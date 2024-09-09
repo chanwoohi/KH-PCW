51bdd2a4271e8b956a509d28aca674cb79ed833a
ParkChanWooo <chanwoohi@naver.com>
2024-09-09 10:33:19 +0900

    리액트 1일차 : 리액트 프로젝트 생성 및 Button 컴포넌트 생성

---
+++ react/react1/src/App.js
@@ -0,0 +1,14 @@
+// import './css/App.css' // css 폴더에 App.css가 있고, 연결할 때 사용
+
+import Button from "./button";
+
+function App() {
+  return (
+    <div>
+      <button>버튼</button>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
