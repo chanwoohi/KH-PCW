7bf15e9d2dcdb8502638713ffb6dec39987108c9
ParkChanWooo <chanwoohi@naver.com>
2024-09-10 10:18:30 +0900

    리액트 2일차 : 페이지 이동 버튼 구현

--- react/test1/src/App.js
+++ react/test1/src/App.js
@@ -1,10 +1,16 @@
+import ButtonBox from "./buttonEx/ButtonBox.js";
 import Test1 from "./test1/Test1";
+import Todo from "./todo/Todo";
+import TodoList from "./todoList/TodoList.js";
 
 function App() {
   return(
     <div>
       {/* <Test1 /> */}
-    </div>
+      {/* <Todo/> */}
+      {/* <TodoList/> */}
+    <ButtonBox/>
+    <div/></div>
   );
 }
 
