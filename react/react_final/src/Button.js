0ba0edef2b588d6c111569e06d8aee2083b46738
ParkChanWooo <chanwoohi@naver.com>
2024-09-23 15:20:25 +0900

    스프링 최종 정리 (시험)

---
+++ react/react_final/src/Button.js
@@ -0,0 +1,32 @@
+function Button1(props){
+	return (
+		<button
+			onClick={props.click}
+			className={props.classNames}>
+				{props.text}
+			</button>
+	)
+}
+
+const Button2 = ({text, styles, click})=>{
+	return(
+		<button 
+			onClick={click}
+			style={styles}>
+				{text}
+		</button>
+	)
+}
+
+const Button3 = ({text, styles, click})=>{
+
+	return(
+		<button 
+			onClick={click}
+			style={styles}>
+				{text}
+		</button>
+	)
+}
+
+export {Button1, Button2, Button3};
